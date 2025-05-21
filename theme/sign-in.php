402e5caf8cc1c16fb758479e1da969347e507665
vareschi <derick@vareschi.com.br>
2025-05-21 13:49:31 -0300

    Login Ajuste CSS

--- theme/sign-in.php
+++ theme/sign-in.php
@@ -52,20 +52,36 @@
                 <div class="alert alert-danger">E-mail ou senha inválidos.</div>
               <?php endif; ?>
               
-              <form method="POST" action="auth.php">
-                <div class="form-group mb-4">
-                  <input type="email" name="email" class="form-control input-lg" placeholder="E-mail" required>
+              <form action="/index.html">
+                <div class="row">
+                  <div class="form-group col-md-12 mb-4">
+                    <input type="email" class="form-control input-lg" id="email" aria-describedby="emailHelp" placeholder="Username">
+                  </div>
+
+                  <div class="form-group col-md-12 ">
+                    <input type="password" class="form-control input-lg" id="password" placeholder="Password">
+                  </div>
+
+                  <div class="col-md-12">
+                    <div class="d-flex my-2 justify-content-between">
+                      <div class="d-inline-block mr-3">
+                        <label class="control control-checkbox">Remember me
+                          <input type="checkbox" />
+                          <div class="control-indicator"></div>
+                        </label>
+                      </div>
+
+                      <p class="mt-4">Esqueceu sua senha? <a class="text-blue" href="#">Recuperar</a></p>
+                    </div>
+
+                    <button type="submit" class="btn btn-lg btn-primary btn-block mb-4">Login</button>
+
+                    <p>Não tem uma conta?
+                      <a class="text-blue" href="sign-up.html">Cadastrar-se</a>
+                    </p>
+                  </div>
                 </div>
-                <div class="form-group mb-4">
-                  <input type="password" name="senha" class="form-control input-lg" placeholder="Senha" required>
-                </div>
-                <div class="form-check mb-4">
-                  <input type="checkbox" class="form-check-input" id="rememberMe">
-                  <label class="form-check-label" for="rememberMe">Lembrar-me</label>
-                </div>
-                <button type="submit" class="btn btn-primary btn-lg btn-block">Entrar</button>
               </form>
-              <p class="mt-4">Esqueceu sua senha? <a class="text-blue" href="#">Recuperar</a></p>
             </div>
           </div>
         </div>
