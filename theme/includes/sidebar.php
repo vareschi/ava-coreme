703c20aec7355c1071cbc42910c33a47da97ebf2
vareschi <derick@vareschi.com.br>
2025-06-02 14:54:20 -0300

    Alterações Menu e Topbar

--- theme/includes/sidebar.php
+++ theme/includes/sidebar.php
@@ -19,7 +19,7 @@
             <div class="" data-simplebar style="height: 100%;">
               <!-- sidebar menu -->
               <ul class="nav sidebar-inner" id="sidebar-menu">
-                <li class="has-sub active expand">
+                <li class="has-sub">
                   <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#dashboard"
                     aria-expanded="false" aria-controls="dashboard">
                     <i class="mdi mdi-view-dashboard-outline"></i>
