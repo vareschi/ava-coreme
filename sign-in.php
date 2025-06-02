2a3f5196bd95514da4396b1fc4d5090bbc4fa5f3
vareschi <derick@vareschi.com.br>
2025-06-02 15:20:19 -0300

    Ajuste Projeto Raiz

--- sign-in.php
+++ sign-in.php
@@ -1,4 +1,11 @@
-<?php session_start(); ?>
+<?php session_start(); 
+
+if (isset($_SESSION['usuario_logado']) && $_SESSION['usuario_logado'] === true) {
+    header("Location: users.php");
+    exit();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
