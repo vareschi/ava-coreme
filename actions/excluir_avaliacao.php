5555ccbd63c4dd683caf00fbb4c09bde4e55d005
vareschi <derick@vareschi.com.br>
2025-06-03 15:55:37 -0300

    Cadastro de Avaliações

--- actions/excluir_avaliacao.php
+++ actions/excluir_avaliacao.php
@@ -6,6 +6,11 @@ if (!isset($_SESSION['usuario_id'])) {
     exit;
 }
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 require_once 'includes/config.php';
 require_once 'includes/funcoes.php';
 
