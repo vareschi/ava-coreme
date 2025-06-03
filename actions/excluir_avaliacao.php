12a330ccabf547d7ac9b9fff1780e243293fdcae
vareschi <derick@vareschi.com.br>
2025-06-03 15:56:42 -0300

    Cadastro de Avaliações

--- actions/excluir_avaliacao.php
+++ actions/excluir_avaliacao.php
@@ -11,8 +11,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-require_once 'includes/config.php';
-require_once 'includes/funcoes.php';
+require_once '../includes/config.php';
+require_once '../includes/funcoes.php';
 
 verificarAcessoRecurso('avaliacoes');
 
