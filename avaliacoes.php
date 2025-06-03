09631d002d1a14f4cea1951635aa6469823ae898
vareschi <derick@vareschi.com.br>
2025-06-03 12:05:09 -0300

    Ajuste Projeto Raiz

--- avaliacoes.php
+++ avaliacoes.php
@@ -109,6 +109,6 @@ $avaliacoes = $pdo->query("SELECT a.*, e.nome AS especialidade
   </div>
 </div>
 
-<!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 <?php include 'includes/footer.php'; ?>
