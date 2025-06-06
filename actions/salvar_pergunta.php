66b16a1b1c5454512461e49f4408e7f72eb9d7f6
vareschi <derick@vareschi.com.br>
2025-06-06 00:00:17 -0300

    Cadastro de Avaliações

--- actions/salvar_pergunta.php
+++ actions/salvar_pergunta.php
@@ -14,8 +14,8 @@ if (!isset($_SESSION['usuario_id'])) {
     exit;
 }
 
-require_once 'includes/config.php';
-require_once 'includes/funcoes.php';
+require_once '../includes/config.php';
+require_once '../includes/funcoes.php';
 
 verificarAcessoRecurso('avaliacoes');
 
