ad328dc20e78fce43db72949d7489d3451222e50
vareschi <derick@vareschi.com.br>
2025-06-05 23:59:16 -0300

    Cadastro de Avaliações

--- actions/salvar_pergunta.php
+++ actions/salvar_pergunta.php
@@ -1,5 +1,13 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
+
+
+
 
 if (!isset($_SESSION['usuario_id'])) {
     header("Location: sign-in.php");
