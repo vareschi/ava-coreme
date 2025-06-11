efcbc0c4f5c6463f89e830fd5fc3e2bde7362a49
vareschi <derick@vareschi.com.br>
2025-06-11 14:36:58 -0300

    Página Avaliar

--- auth.php
+++ auth.php
@@ -25,12 +25,11 @@ if ($usuario && password_verify($senha, $usuario['senha'])) {
     $_SESSION['usuario_id'] = $usuario['id'];
     $_SESSION['usuario_nome'] = $usuario['nome'];
 
-    // 5. Carrega os perfis do usuário
+    // 5. Carrega os **IDs** dos perfis do usuário
     $stmtPerfis = $pdo->prepare("
-        SELECT p.nome 
-        FROM usuario_perfis up 
-        JOIN perfis p ON up.perfil_id = p.id 
-        WHERE up.usuario_id = ?
+        SELECT perfil_id 
+        FROM usuario_perfis  
+        WHERE usuario_id = ?
     ");
     $stmtPerfis->execute([$usuario['id']]);
     $_SESSION['perfis'] = $stmtPerfis->fetchAll(PDO::FETCH_COLUMN);
