af391a9724b09f0f5b140553fdbcfb07b9891c28
vareschi <derick@vareschi.com.br>
2025-06-13 18:46:08 -0300

    Ajuste Index

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
 
-// Se o usuário não estiver logado, redireciona para o sign-in do subdomínio atual
+// Se o usuário NÃO estiver logado, redireciona para o sign-in do subdomínio atual
 if (!isset($_SESSION['usuario_id'])) {
     $host = $_SERVER['HTTP_HOST']; // Captura o domínio ou subdomínio atual
     header("Location: https://{$host}/sign-in.php");
     exit;
 }
 
-
+// Se estiver logado, redireciona para a página de usuários
+header("Location: users.php");
+exit;
