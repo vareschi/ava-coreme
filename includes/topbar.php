1bfa2731cc212dc1dabf6013315970809906df00
vareschi <derick@vareschi.com.br>
2025-06-13 15:13:51 -0300

    avaliacoes.php para avaliacoes_modelo

--- includes/topbar.php
+++ includes/topbar.php
@@ -503,20 +503,17 @@ if ($usuario_id) {
 
                       <li>
                         <a href="user-profile.html">
-                          <i class="mdi mdi-account"></i> My Profile
+                          <i class="mdi mdi-account"></i> Perfil
                         </a>
                       </li>
                       <li>
                         <a href="#">
-                          <i class="mdi mdi-email"></i> Message
+                          <i class="mdi mdi-email"></i> Mensagens
                         </a>
                       </li>
-                      <li>
-                        <a href="#"> <i class="mdi mdi-diamond-stone"></i> Projects </a>
-                      </li>
-                      <li class="right-sidebar-in">
-                        <a href="javascript:0"> <i class="mdi mdi-settings"></i> Setting </a>
-                      </li>
+                      <!-- <li class="right-sidebar-in">
+                        <a href="javascript:0"> <i class="mdi mdi-settings"></i> Configuração </a>
+                      </li> -->
 
                       <li class="dropdown-footer">
                         <a href="logout.php"> <i class="mdi mdi-logout"></i> Log Out </a>
