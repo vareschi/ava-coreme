543bf7d26b9f4db38dfc6c68be77fabd21be3f9e
vareschi <derick@vareschi.com.br>
2025-06-13 15:17:32 -0300

    avaliacoes.php para avaliacoes_modelo

--- includes/topbar.php
+++ includes/topbar.php
@@ -23,13 +23,13 @@ if ($usuario_id) {
           <!-- Header -->
           <header class="main-header " id="header">
             <nav class="navbar navbar-static-top navbar-expand-lg">
-              <!-- Sidebar toggle button 
+              <!-- Sidebar toggle button -->
               <button id="sidebar-toggler" class="sidebar-toggle">
                 <span class="sr-only">Toggle navigation</span>
               </button>
-              <!-- search form 
+              <!-- search form -->
               <div class="search-form d-none d-lg-inline-block">
-                <div class="input-group">
+                <!--<div class="input-group">
                   <button type="button" name="search" id="search-btn" class="btn btn-flat">
                     <i class="mdi mdi-magnify"></i>
                   </button>
@@ -38,8 +38,8 @@ if ($usuario_id) {
                 </div>
                 <div id="search-results-container">
                   <ul id="search-results"></ul>
-                </div>
-              </div> -->
+                </div>-->
+              </div> 
 
               <div class="navbar-right ">
                 <ul class="nav navbar-nav">
