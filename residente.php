5913a5f2aecb1a2d5b021c37e462a71b9eeb34ab
vareschi <derick@vareschi.com.br>
2025-06-23 17:45:18 -0300

    Documentos Residente

--- residente.php
+++ residente.php
@@ -1,10 +1,20 @@
+<link href="assets/options/optionswitch.css" rel="stylesheet">
 <?php
-require_once 'includes/header.php';
+session_start();
+
+if (!isset($_SESSION['usuario_id'])) {
+    header("Location: sign-in.php");
+    exit;
+}
+
 require_once 'includes/config.php';
+require_once 'includes/funcoes.php';
 
 include 'includes/header.php';
 include 'includes/sidebar.php';
 include 'includes/topbar.php';
+
+verificarAcessoRecurso('usuarios');
 
 $pdo = getPDO();
 
