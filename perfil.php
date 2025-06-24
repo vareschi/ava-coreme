237a7438002d0cba4aa6907e936a93262b30b04d
vareschi <derick@vareschi.com.br>
2025-06-24 10:39:45 -0300

    Documentos Residente

--- perfil.php
+++ perfil.php
@@ -82,16 +82,71 @@ include 'includes/topbar.php';
       <div class="profile-content-right profile-right-spacing py-5">
         <ul class="nav nav-tabs px-3 px-xl-5 nav-style-border" id="myTab" role="tablist">
           <li class="nav-item">
-            <a class="nav-link" id="arquivos-tab" data-toggle="tab" href="#arquivos" role="tab" aria-controls="arquivos" aria-selected="false">Arquivos</a>
+            <a class="nav-link active" id="settings-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="false">Usuário</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link active" id="settings-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="false">Usuário</a>
+            <a class="nav-link" id="arquivos-tab" data-toggle="tab" href="#arquivos" role="tab" aria-controls="arquivos" aria-selected="false">Arquivos</a>
           </li>
         </ul>
 
         <div class="tab-content px-3 px-xl-5" id="myTabContent">
           <div class="tab-pane fade show active" id="settings" role="tabpanel" aria-labelledby="settings-tab">
+            <div class="tab-pane-content mt-5">
+              <form action="actions/atualizar_perfil.php" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="usuario_id" value="<?= $usuario_id ?>">
+
+                <div class="form-group row mb-6">
+                  <label for="coverImage" class="col-sm-4 col-lg-2 col-form-label">Imagem Perfil</label>
+                  <div class="col-sm-8 col-lg-10">
+                    <div class="custom-file mb-1">
+                      <input type="file" class="custom-file-input" name="imagem_perfil" id="coverImage">
+                      <label class="custom-file-label" for="coverImage">Carregar Imagem...</label>
+                      <div class="invalid-feedback">Imagem invalida</div>
+                    </div>
+                  </div>
+                </div>
+
+                    <div class="form-group mb-4">
+                      <label for="firstName">nome Completo</label>
+                      <input type="text" class="form-control" name="nome" value="<?= htmlspecialchars($usuario['nome'] ?? '') ?>">
+                    </div>
+                    <div class="form-group mb-4">
+                      <label for="email">Email</label>
+                      <input type="email" class="form-control" name="email" value="<?= htmlspecialchars($usuario['email'] ?? '') ?>">
+                    </div>
+
+                <div class="row mb-2">
+                  <div class="col-lg-6">
+                    <div class="form-group mb-4">
+                      <label for="oldPassword">Senha Antiga</label>
+                      <input type="password" class="form-control" name="senha_atual">
+                    </div>
+
+                    <div class="form-group mb-4">
+                      <label for="newPassword">Nova Senha</label>
+                      <input type="password" class="form-control" name="nova_senha">
+                    </div>
+
+                    <div class="form-group mb-4">
+                      <label for="conPassword">Confirme a Nova Senha</label>
+                      <input type="password" class="form-control" name="confirmar_senha">
+                    </div>
+                  </div>
+                </div>
+
+
+
+                
+
+                <div class="d-flex justify-content-end mt-5">
+                  <button type="submit" class="btn btn-primary mb-2 btn-pill">Atualizar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <!-- Arquivos -->
+          <div class="tab-pane fade show active" id="arquivos" role="tabpanel" aria-labelledby="arquivos-tab">
             <div class="tab-pane-content mt-5">
               <form action="actions/atualizar_perfil.php" method="post" enctype="multipart/form-data">
                 <input type="hidden" name="usuario_id" value="<?= $usuario_id ?>">
